2706501093628b03b1636278b400ab86198eb2f2
Adam Naamani <adamnaamani@gmail.com>
2019-05-01 12:17:40 -0700

    Map component receives props from Dashboard

--- app/javascript/components/accounts/Dashboard.jsx
+++ app/javascript/components/accounts/Dashboard.jsx
@@ -3,32 +3,43 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getAccounts } from '../../actions/accounts';
+import { getCoordinates } from '../../actions/geo';
 import Map from '../geo/Map'
 
 class Dashboard extends Component {
 	static propTypes = {
 		accounts: PropTypes.array,
-		getAccounts: PropTypes.func.isRequired
+		coordinates: PropTypes.array,
+		getAccounts: PropTypes.func.isRequired,		
+		getCoordinates: PropTypes.func.isRequired		
 	}	
 	constructor(props) {
 		super(props)
 		this.initialState = {
-			accounts: []
+			accounts: this.props.accounts || [],
+			coordinates: this.props.coordinates || []
     };
     this.state = this.initialState;
   }
   componentDidMount() {
-  	this.props.getAccounts();	
+  	this.props.getAccounts().then(_=> {
+			this.props.accounts.forEach(account => {
+				this.props.getCoordinates(account, [account.bank_address, account.bank_location].join(', '));
+			})  		
+  	});	
   }
   componentDidUpdate(prevProps, prevState) {
 	  if(prevProps.accounts !== this.props.accounts) {
 	    this.setState({accounts: this.props.accounts}); 	    
-	  }	    	
+	  }
+	  if(prevProps.coordinates !== this.props.coordinates) {
+	    this.setState({coordinates: this.props.coordinates});
+	  }	  	    	
   }
   render() {
   	return (
   		<Fragment>
-  			<Map />
+  			<Map accounts={this.state.accounts} coordinates={this.state.coordinates} />
   		</Fragment>
   	)
   }
@@ -36,13 +47,15 @@ class Dashboard extends Component {
 
 function mapStateToProps(state) {
 	return {
-		accounts: state.accounts.accounts
+		accounts: state.accounts.accounts,
+		coordinates: state.geo.coordinates
 	}
 }
 
 function mapDispatchToProps(dispatch) {
 	return {
-		getAccounts: bindActionCreators(getAccounts, dispatch)
+		getAccounts: bindActionCreators(getAccounts, dispatch),
+		getCoordinates: bindActionCreators(getCoordinates, dispatch)
 	}
 }
 
